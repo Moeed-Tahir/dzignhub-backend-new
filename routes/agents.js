63c0cfd01e003599d70488bdfc124dd0a57f1ffe
Avik khan <75199596+itspsychocoder@users.noreply.github.com>
2025-07-23 20:28:07 +0500

    addseo agent

--- routes/agents.js
+++ routes/agents.js
@@ -5,6 +5,7 @@ const logoDesigner = require('../controller/agents/logo-designer');
 const contentCreation = require('../controller/agents/content-creation');
 const updateBrandDesignData = require('../controller/agents/update-brand-design-data');
 const getBrandDesignData = require('../controller/agents/get-brand-designer-data');
+const noviSeoAgent = require('../controller/agents/novi-seo-agent');
 
 
 // Routes
@@ -13,5 +14,6 @@ router.route('/agents/logo-designer').post(logoDesigner);
 router.route('/agents/content-creation').post(contentCreation);
 router.route('/agents/update-brand-design-data').post(updateBrandDesignData);
 router.route('/agents/get-brand-designer-data').post(getBrandDesignData);
+router.route('/agents/novi-seo-agent').post(noviSeoAgent);
 
 module.exports = router;
